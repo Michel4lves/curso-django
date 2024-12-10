1d51c72784b625165b9f52be83fcb9ee2dbd5510
michel4alves <michelsantosa@gmail.com>
2024-12-10 19:10:49 -0300

    decouple to databases

--- cursodjango/settings.py
+++ cursodjango/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from functools import partial
+import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 
@@ -22,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-# 'django-insecure-nkj^1af$1)d=^dj5i9dwr#_+n!w=in1eqauh@d3koy6&npu)!c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
@@ -76,10 +77,13 @@ WSGI_APPLICATION = 'cursodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR / 'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
     }
 }
 
