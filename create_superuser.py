946551139e43608070ea55eec71e17fdcb895f82
michel4alves <michelsantosa@gmail.com>
2024-12-16 22:31:35 -0300

    add auto super user fixed 03

--- create_superuser.py
+++ create_superuser.py
@@ -13,14 +13,14 @@ django.setup()
 User = get_user_model()
 
 # Configurações do superusuário
-SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME', 'admin')
+# SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME', 'admin')
 SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
 SUPERUSER_PASSWORD = os.getenv('SUPERUSER_PASSWORD', 'adminpassword')
 
 if not User.objects.filter(email=SUPERUSER_EMAIL).exists():
 # if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
     User.objects.create_superuser(
-        username=SUPERUSER_USERNAME,
+        # username=SUPERUSER_USERNAME,
         email=SUPERUSER_EMAIL,
         password=SUPERUSER_PASSWORD,
     )
