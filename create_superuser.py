f93da8f4a3715798429bfe077068dd29a2fe8901
michel4alves <michelsantosa@gmail.com>
2024-12-16 21:44:17 -0300

    add auto super user

---
+++ create_superuser.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+import os
+
+"""
+    Criar um super usuário automaticamente no deploy.
+"""
+
+# Configurar as variáveis de ambiente
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cursodjango.settings')
+import django
+django.setup()
+
+User = get_user_model()
+
+# Configurações do superusuário
+# SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME', 'admin')
+SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL', 'admin@example.com')
+SUPERUSER_PASSWORD = os.getenv('SUPERUSER_PASSWORD', 'adminpassword')
+
+if not User.objects.filter(username=SUPERUSER_EMAIL).exists():
+# if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+    User.objects.create_superuser(
+        # username=SUPERUSER_USERNAME,
+        email=SUPERUSER_EMAIL,
+        password=SUPERUSER_PASSWORD,
+    )
+    print("Superusuário criado com sucesso!")
+else:
+    print("O superusuário já existe.")
